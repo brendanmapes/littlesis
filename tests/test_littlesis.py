9a24f1c5b7e3aaa12ced54b5b4eb4a405e617d8c
Brendan Mapes <75643602+brendanmapes@users.noreply.github.com>
2021-12-16 16:16:07 -0500

    Update test_littlesis.py

--- tests/test_littlesis.py
+++ tests/test_littlesis.py
@@ -1,1 +1,26 @@
+import littlesis
 from littlesis import littlesis
+
+def test_name_to_id():
+    example = 'Barack Obama'
+    expected = 13503
+    actual = littlesis.name_to_id(example)
+    assert actual == expected
+    
+def test_name_to_id_2():
+    example = 'Obama'
+    expected = 13503
+    actual = littlesis.name_to_id(example)
+    assert actual == expected 
+    
+def test_name_to_id_3():
+    example = 'obama'
+    expected = 13503
+    actual = littlesis.name_to_id(example)
+    assert actual == expected
+    
+def test_name_to_id_4():
+    example = 'BARACK OBAMA'
+    expected = 13503
+    actual = littlesis.name_to_id(example)
+    assert actual == expected 
